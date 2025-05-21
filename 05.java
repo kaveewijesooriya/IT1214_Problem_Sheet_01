c771ae2572ad1e4d944b62e6b4e7ccb142d7bb17
kaveewijesooriya <kaveeshajanani0@gmail.com>
2025-05-21 18:32:52 +0000

    5th answer

---
+++ 05.java
@@ -0,0 +1,50 @@
+class Student {
+    private String name;
+    private int exam1;
+    private int exam2;
+    private int exam3;
+
+    public Student(String name, int exam1, int exam2, int exam3) throws IllegalArgumentException {
+        if (exam1 < 0 || exam1 > 100 || exam2 < 0 || exam2 > 100 || exam3 < 0 || exam3 > 100) {
+            throw new IllegalArgumentException("Exam scores must be between 0 and 100.");
+        }
+        
+        this.name = name;
+        this.exam1 = exam1;
+        this.exam2 = exam2;
+        this.exam3 = exam3;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getExam1() {
+        return exam1;
+    }
+
+    public int getExam2() {
+        return exam2;
+    }
+
+    public int getExam3() {
+        return exam3;
+    }
+
+
+    public double calculateAverage() {
+        return (exam1 + exam2 + exam3) / 3.0;
+    }
+
+    public static void main(String[] args) {
+        try {
+            Student student = new Student("John", 75, 110, 90);
+            
+            System.out.println("Student: " + student.getName()+ student.calculateAverage());
+        } catch (IllegalArgumentException e) {
+
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
